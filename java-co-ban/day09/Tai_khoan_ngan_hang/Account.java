becf08a65358ff7453cbef1b94042ca9ac487b1b
ngohinh99 <ngohinh99@gmail.com>
2022-08-16 20:27:59 +0700

    day09

--- java-co-ban/day09/Tai_khoan_ngan_hang/Account.java
+++ java-co-ban/day09/Tai_khoan_ngan_hang/Account.java
@@ -14,6 +14,11 @@ public class Account implements IAccount{
         this.email = email;
         this.accountBalance = accountBalance;
     }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+    
     @Override
     public void recharge(double amount) {
         // TODO Auto-generated method stub
@@ -31,60 +36,6 @@ public class Account implements IAccount{
     }
 
 
-    /**
-     * @return String return the name
-     */
-    public String getName() {
-        return name;
-    }
 
-    /**
-     * @param name the name to set
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    /**
-     * @return String return the accountNumber
-     */
-    public String getAccountNumber() {
-        return accountNumber;
-    }
-
-    /**
-     * @param accountNumber the accountNumber to set
-     */
-    public void setAccountNumber(String accountNumber) {
-        this.accountNumber = accountNumber;
-    }
-
-    /**
-     * @return String return the email
-     */
-    public String getEmail() {
-        return email;
-    }
-
-    /**
-     * @param email the email to set
-     */
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    /**
-     * @return double return the accountBalance
-     */
-    public double getAccountBalance() {
-        return accountBalance;
-    }
-
-    /**
-     * @param accountBalance the accountBalance to set
-     */
-    public void setAccountBalance(double accountBalance) {
-        this.accountBalance = accountBalance;
-    }
 
 }
